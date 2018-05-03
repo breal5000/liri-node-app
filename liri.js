cbc296dd7fabf2459a57c101fa599e97d5912f97
Brandon Frazier <breal5000@gmail.comgit config --global user.name Brandon>
2018-05-02 20:18:01 -0700

    final changes

--- liri.js
+++ liri.js
@@ -1,14 +1,141 @@
 console.log("the bot is starting")
-// require method
-var dotEnv = require ("dotEnv").config();
+
+//require("dotenv").config();
+
+var Twitter = require("twitter");
+
+var Spotify = require("node-spotify-api");
+
+var keys = require("./keys");
+
+var request = require("request");
+
+var fs = require("fs");
 
 var spotify = new Spotify(keys.spotify);
+// FUNCTION
 
-var client = new Twitter(keys.twitter);
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+ 
+var client = new Twitter({
+  consumer_key: 'jroTXU1dgClobCfpETmcasKDq',
+  consumer_secret: 'WspEMxmYddcTS7riGeeOcqXpRSK7qKAOs3Ux6DcW9nIRdatuSG',
+  access_token_key: '991150617845415941-6zGGELNCsctVS9HOKEnpvPc1hBuH9rb',
+  access_token_secret: 'xtBFzFXatJ99rybbaI6JjXTdmfizoGzmPnoS2E3uPUCuz'
+});
 
-var twit = require("twit")
+var getMyTweets = function() {
+  var client = new Twitter(keys.twitter);
+  var params = {
+    screen_name: "Brandon Frazier"
+  };
+  client.get("statuses/user_timeline", params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  });
+}; 
 
-var config = require("keys")
+// FUNCTIONS
 
+var getArtistNames = function(artist) {
+  return artist.name;
+};
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "What's my age again";
+  }
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+      var songs = data.tracks.items;
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
+// Function for running a Movie Search
+var getMeMovie = function(movieName) {
+  if (movieName === undefined) {
+    movieName = "Commando";
+  }
+  var urlHit =
+    "http://www.omdbapi.com/?t=" +
+    movieName +
+    "&y=&plot=full&tomatoes=true&apikey=trilogy";
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var jsonData = JSON.parse(body);
+      console.log("Title: " + jsonData.Title);
+      console.log("Year: " + jsonData.Year);
+      console.log("Rated: " + jsonData.Rated);
+      console.log("IMDB Rating: " + jsonData.imdbRating);
+      console.log("Country: " + jsonData.Country);
+      console.log("Language: " + jsonData.Language);
+      console.log("Plot: " + jsonData.Plot);
+      console.log("Actors: " + jsonData.Actors);
+      console.log("Rotton Tomatoes Rating: " + jsonData.Ratings[1].Value);
+    }
+  });
+};
+// Function for running a command based on text file
+var doWhatItSays = function() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    console.log(data);
+    var dataArr = data.split(",");
+    if (dataArr.length === 2) {
+      pick(dataArr[0], dataArr[1]);
+    } else if (dataArr.length === 1) {
+      pick(dataArr[0]);
+    }
+  });
+};
+// Function to determine which one 
+var pick = function(caseData, functionData) {
+  switch (caseData) {
+    case "my-tweets":
+      getMyTweets();
+      break;
+    case "spotify-this-song":
+      getMeSpotify(functionData);
+      break;
+    case "movie-this":
+      getMeMovie(functionData);
+      break;
+    case "do-what-it-says":
+      doWhatItSays();
+      break;
+    default:
+      console.log("LIRI doesn't know that");
+  }
+};
+// CLI arguments
+var runThis = function(argOne, argTwo) {
+  pick(argOne, argTwo);
+};
 
-
+runThis(process.argv[2], process.argv[3]);
